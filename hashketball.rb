af576b39efd49fc85d072110153e1b10b4c3d304
Dino Kunovac <dinokunovac@gmail.com>
2020-01-04 00:25:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,6 +102,18 @@ def game_hash
   }
 end
 
+require 'pry'
+
+def num_points_scored(player_search)
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:player_name] == player_search
+        return player[:points]
+      end
+    end
+  end
+end
+
 
 
 
